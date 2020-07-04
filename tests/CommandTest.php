70d14d259c8de41d52178d91e812946b6dedbde7
Bruce Brophy <bruce@brucebrophy.com>
2020-07-04 00:26:40 -0400

    Sets up TestCase

---
+++ tests/CommandTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace BruceBrophy\LaravelBlog\Test;
+
+class CommandTest extends TestCase
+{
+	public function testCommandCanPublishViews()
+	{
+		$this->assertTrue(true);
+	}
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace BruceBrophy\LaravelBlog\Test;
+
+use BruceBrophy\LaravelBlog\BlogServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+class TestCase extends Orchestra
+{
+	protected function getEnvironmentSetUp($app)
+	{
+		include_once __DIR__.'/../database/migrations/create_posts_table.php.stub';
+
+		(new \CreatePostsTable)->up();
+	}
+
+	protected function getPackageProviders($app)
+	{
+		return [
+			BlogServiceProvider::class,
+		];
+	}
+}
