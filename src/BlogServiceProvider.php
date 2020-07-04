2cfa2875f207274b83cc5b50cfc00508dcbb1605
Bruce Brophy <bruce@brucebrophy.com>
2020-07-03 22:49:03 -0400

    Adds command to publish stubs

--- src/BlogServiceProvider.php
+++ src/BlogServiceProvider.php
@@ -3,13 +3,17 @@
 namespace BruceBrophy\LaravelBlog;
 
 use Illuminate\Support\ServiceProvider;
-use BruceBrophy\LaravelBlog\Contracts\Post as PostContract;
+use BruceBrophy\LaravelBlog\Console\Commands\PublishViews;
 
 class BlogServiceProvider extends ServiceProvider
 {
 	public function boot()
 	{
-		$this->app->bind(PostContract::class);
+		if ($this->app->runningInConsole()) {
+			$this->commands([
+				PublishViews::class,
+			]);
+		}
 
 		$this->publishes([
 			__DIR__.'/../config/blog.php' => config_path('blog.php'),
@@ -18,7 +22,5 @@ class BlogServiceProvider extends ServiceProvider
 		$this->publishes([
 			__DIR__.'/../database/migrations/create_posts_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_posts_table.php'),
 		], 'migrations');
-
-		$this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-blog');
 	}
 }
---
+++ src/Console/PublishViews.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace BruceBrophy\LaravelBlog\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+
+class PublishViews extends Command
+{
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'blog:publish {--ui=tailwind}';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Publish blog view resources';
+
+	public function handle(Filesystem $filesystem)
+	{
+		$uiLibrary = $this->option('ui');
+		$filesystem->copyDirectory(__DIR__.'/../../resources/views/' . $uiLibrary . '/blog', resource_path('views/vendor/laravel-blog'));
+	}
+}
