c7d03a03ba46a5c610ef60f8261b53102d432e9d
Bruce Brophy <bruce@brucebrophy.com>
2020-07-05 16:52:24 -0400

    Adds Tailwind Layout File

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use BruceBrophy\LaravelBlog\Http\Controllers\PostController;
 
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
-Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-Route::patch('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
-Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+Route::get('/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::get('/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+Route::patch('/{post}', [PostController::class, 'update'])->name('posts.update');
+Route::delete('/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
